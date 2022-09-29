f27955c7d7510a4a5cb391da02de7dfe51ff238d
natecole <natelcole88@gmail.com>
2022-09-29 12:07:43 -0700

    REST API finished

--- backend/backend.js
+++ backend/backend.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const express = require('express');
 const app = express();
 const port = 8675;
@@ -40,19 +41,75 @@ app.get('/', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
-   const name = req.query.name;
-   if (name != undefined){
-       let result = findUserByName(name);
+   const name = req.query.name,
+         job = req.query.job;
+
+   let result = Array.from(new Set(findUserByName(name).concat(findUserByJob(job))));
+
+   if (result.length > 0 && (name != undefined || job != undefined)){
+      result = {users_list: result};
+      res.send(result);
+   }
+   else{
+      res.send(users);
+   }
+});
+
+app.get('/users/:id', (req, res) => {
+   const id = req.params['id']; //or req.params.id
+   let result = findUserById(id);
+   if (result === undefined || result.length == 0)
+       res.status(404).send('Resource not found.');
+   else {
        result = {users_list: result};
        res.send(result);
    }
-   else{
-       res.send(users);
+});
+
+app.post('/users', (req, res) => {
+   const userToAdd = req.body;
+   addUser(userToAdd);
+   res.status(200).end();
+});
+
+app.delete('/users/:id', (req, res) => {
+   const id = req.params['id']; //or req.params.id
+   if (deleteUser(id)){
+      res.status(200).end();
+   }
+   else {
+      res.status(404).send('Id not found.');
    }
 });
 
 const findUserByName = (name) => { 
    return users['users_list'].filter( (user) => user['name'] === name); 
+}
+
+const findUserByJob = (job) => { 
+   return users['users_list'].filter( (user) => user['job'] === job); 
+}
+
+function findUserById(id) {
+   return users['users_list'].find( (user) => user['id'] === id); // or line below
+   //return users['users_list'].filter( (user) => user['id'] === id);
+}
+
+function addUser(user){
+   users['users_list'].push(user);
+}
+
+function deleteUser(id){
+   const index = users['users_list'].indexOf(
+      findUserById(id)
+   );
+   if (index > -1) {
+      users['users_list'].splice(index, 1);
+      return true;
+   }
+   else {
+      return false;
+   }
 }
 
 app.listen(port, () => {
